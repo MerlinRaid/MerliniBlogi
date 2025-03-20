4da512b714133a2f960bc46e0f9e924846275d95
Merlinraid <merlin.raid@gmail.com>
2025-03-20 15:31:01 +0200

    blogi loomine/navbar

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+$page = isset($_GET['page']) ? $_GET['page'] : 'homepage' ; #Kui olema page siis võtab selle, kui ei siis võtab index faili
+$allowed_pages = ['homepage', 'blog', 'contact', 'post1'. 'post2', 'psot2']; //.html failid
+if(!in_array($page, $allowed_pages)){
+    $page = 'homepage';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Simple</title>
+</head>
+<body>
+    <div class="container">
+        <?php include 'navbar.html'; ?>
+    </div>
+  
+    <div class="container">
+        <?php include("$page.html") ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html> 
